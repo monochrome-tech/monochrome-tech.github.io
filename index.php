b46e92c3ca714bda0a5cde07c965381e3069cc49
the-infinite <toby.johnson.precious@gmail.com>
2023-08-03 19:40:09 +0100

    Added: navbar

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<base href="/">
+
+	<meta charset="UTF-8">
+	<meta content="IE=Edge" http-equiv="X-UA-Compatible">
+	<meta name="description"
+		content="We create and/or market your digital products for your business to carry you to the next level using the absolute best technologies and a user centered approach because, your business is our business, and your business is about the users.">
+
+	<!-- 
+	You are The Innovative and we are The Creative. We help you create and/or market your digital products for your business to carry you to the next level using the absolute best technologies and design principles using the user-centered approach because — let's face it — it's all about you! We perform software development, product analysis, user experience analytics, customer experience management, product testing, and so on, with our experienced team of dedicated, talented, and hardworking staff. Our desire is to not just put your products or services out there the way you want us to, but to exceeed the bars you set and create new standards in the industry
+	-->
+
+	<!-- For the font awesome icons -->
+	<script defer src="./js/fontawesome.js"></script>
+	<script defer src="./js/brands.js"></script>
+	<script defer src="./js/solid.js"></script>
+
+	<!-- Favicon -->
+	<link rel="icon" type="image/png" href="favicon.ico" />
+
+	<!-- For the CSS of the project -->
+	<link rel="stylesheet" href="res/style.css">
+	<title>Monochrome Tech.</title>
+</head>
+
+<body>
+	<!-- The header for the Webpage -->
+	<header class="navbar inno">
+		<div class="nav-controls inno">
+			<a href="#navbar">
+				<img 
+					src="./res/logo-dark.png"
+					alt="Limestone's Logo"
+					class="logo"
+					height="44"
+					width="281"/>
+			</a>
+
+			<button class="menuButton inno">
+				<i class="fas fa-bars-staggered fa-2xl"></i>
+				<i class="fas fa-xmark fa-2xl"></i>
+			</button>
+		</div>
+
+		<div class="nav-controls-2">
+			<div class="inno-switch inno">
+				<div class="inno-switch-guard inno">
+					<i class="fas fa-lightbulb"></i>
+				</div>
+			</div>
+
+			<div class="nav-links inno">
+				<a href="#aboutUs">About Us</a>
+				<a href="#solutions">Our Services</a>
+				<a href="#partners">Our Partners</a>
+				<a href="#contactUs">Contact Us</a>
+			</div>
+		</div>
+	</header>
+
+	<section class="home-hero inno">
+		<h1 class="home-hero-title">
+			YOU ARE THE&NewLine;INNOVATIVE
+		</h1>
+	</section>
+
+	<footer>
+	</footer>
+	<script src="./js/main.js"></script>
+</body>
+</html>
