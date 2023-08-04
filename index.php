aa26f08c32e6570835dbd046e603e18454b8dc50
the-infinite <toby.johnson.precious@gmail.com>
2023-08-04 12:33:07 +0100

    Finished: footer and navbar

--- index.php
+++ index.php
@@ -28,46 +28,91 @@
 
 <body>
 	<!-- The header for the Webpage -->
-	<header class="navbar inno">
-		<div class="nav-controls inno">
+	<header class="navbar-group">
+		<div class="navbar inno">
 			<a href="#navbar">
 				<img 
-					src="./res/logo-dark.png"
-					alt="Limestone's Logo"
-					class="logo"
-					height="44"
-					width="281"/>
+					src="./res/logo-light.png"
+					alt="Monochrome Logo"
+					class="monochrome-logo"
+					height="33"
+					width="210.75"/>
 			</a>
 
-			<button class="menuButton inno">
-				<i class="fas fa-bars-staggered fa-2xl"></i>
-				<i class="fas fa-xmark fa-2xl"></i>
-			</button>
-		</div>
+			<div class="nav-navigation">
+				<div class="nav-controls">
+					<div class="inno-switch inno">
+						<div class="inno-switch-guard inno">
+							<i class="inno-switch-guard-button fas fa-lightbulb"></i>
+						</div>
+					</div>
 
-		<div class="nav-controls-2">
-			<div class="inno-switch inno">
-				<div class="inno-switch-guard inno">
-					<i class="fas fa-lightbulb"></i>
+					<div class="toggle-drawer">
+						<i class="menuButton inno fas fa-bars-staggered fa-2xl"></i>
+					</div>
+				</div>	
+				<div class="nav-links inno">
+					<a href="#aboutUs">About Us</a>
+					<a href="#solutions">Our Services</a>
+					<a href="#partners">Our Partners</a>
+					<a href="#contactUs">Contact Us</a>
 				</div>
 			</div>
-
-			<div class="nav-links inno">
-				<a href="#aboutUs">About Us</a>
-				<a href="#solutions">Our Services</a>
-				<a href="#partners">Our Partners</a>
-				<a href="#contactUs">Contact Us</a>
-			</div>
+		</div>
+		<div class="mobile-nav-links inno">
+			<a href="#aboutUs">About Us</a>
+			<a href="#solutions">Our Services</a>
+			<a href="#partners">Our Partners</a>
+			<a href="#contactUs">Contact Us</a>
 		</div>
 	</header>
 
 	<section class="home-hero inno">
 		<h1 class="home-hero-title">
-			YOU ARE THE&NewLine;INNOVATIVE
+			YOU ARE THE
 		</h1>
+		<h1 class="home-hero-subtitle inno">INNOVATIVE</h1>
 	</section>
 
-	<footer>
+	<footer class="inno">
+		<div class="footer-inside">
+			<div class="footer-section">
+				<img
+					src="./res/logo-dark.png"
+					alt="Monochrome Logo"
+					class="footer-monochrome-logo"
+					loading="lazy"
+					height="33"
+					width="210.75"
+				/>
+				<p class="footer-text">
+					Get in touch and discover how we can help you take your brand to the next level — how we can help
+					you transform your concepts into reality.
+				</p>
+			</div>
+
+			<div class="footer-section navigation">
+				<div class="footer-nav-group inno">
+					<p class="footer-title">Quick Links</p>
+					<div class="footer-links inno">
+						<a href="#aboutUs">About Us</a>
+						<a href="#solutions">Our Services</a>
+						<a href="#partners">Our Partners</a>
+						<a href="#contactUs">Contact Us</a>
+					</div>
+				</div>
+				
+				<div class="footer-nav-group">
+					<p class="footer-title">Our Socials</p>
+					<div class="footer-links inno">
+						<a href="#">LinkedIn</a>
+						<a href="#">Twitter</a>
+						<a href="#">Instagram</a>
+					</div>
+				</div>
+			</div>
+		</div>
+        <p class="footer-copyright"><script>document.write("ⓒ" + new Date().getFullYear().toString())</script> <a href="#">Monochrome Tech.</a> All Rights Reserved.</p>
 	</footer>
 	<script src="./js/main.js"></script>
 </body>
---
+++ js/main.js
@@ -0,0 +1,76 @@
+// For managing the themes...
+{
+	// We start out in the innovative theme.
+	var inCreative  = false;
+
+	// The toggle switch.
+	let toggle = document.querySelector(".inno-switch");
+
+	// All elements that are themed on the site.
+	var themed = document.querySelectorAll(".inno");
+
+	toggle.addEventListener('mousedown', function (ev) {
+		let buttonIcon = document.querySelector(".inno-switch-guard-button");
+		let logo = document.querySelector(".monochrome-logo");
+		let footerLogo = document.querySelector(".footer-monochrome-logo");
+		let heroTitle = document.querySelector(".home-hero-title");
+		let heroSubtitle = document.querySelector(".home-hero-subtitle");
+		let drawerButton = document.querySelector(".menuButton");
+
+		toggle.classList.toggle("enabled");
+
+		// Toggle whether or not we are in creative mode.
+		inCreative = !inCreative;
+
+		themed.forEach(function(element) {
+			element.classList.toggle("inno");
+			element.classList.toggle("creative");
+		});
+
+		// Toggle the drawer button theme.
+		drawerButton.classList.toggle("inno");
+		drawerButton.classList.toggle("creative");
+
+		// If we just entered creative mode
+		if(inCreative) {
+			buttonIcon.classList.remove("fa-lightbulb");
+			buttonIcon.classList.add("fa-brain");
+			heroTitle.innerHTML = "WE ARE THE";
+			heroSubtitle.innerHTML = "CREATIVE";
+			logo.src = "../res/logo-dark.png";
+			footerLogo.src = "../res/logo-light.png";
+		}
+
+		else {
+			buttonIcon.classList.add("fa-lightbulb");
+			buttonIcon.classList.remove("fa-brain");
+			logo.src = "../res/logo-light.png";
+			footerLogo.src = "../res/logo-dark.png";
+			heroTitle.innerHTML = "YOU ARE THE";
+			heroSubtitle.innerHTML = "INNOVATIVE";
+		}
+	});
+};
+
+
+{
+	// Now for toggling the navigation.
+	let toggleDrawer = document.querySelector(".toggle-drawer");
+	let links = document.querySelector(".mobile-nav-links");
+
+	// What we CAN do.
+	let active = false;
+
+	toggleDrawer.addEventListener("mousedown", function(ev) {
+		console.log("Toggled: " + active);
+		active = !active;
+
+		if(active) {
+			links.style.display = 'flex';
+		}
+
+		else {
+			links.style.display = 'none';
+		}
+	});
+};
